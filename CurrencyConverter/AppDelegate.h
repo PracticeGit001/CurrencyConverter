f599d71a045942fcd6741d7f10b1b79b6bea8535
Teckchun <Teckchun@teckchuns-macbook-pro.local>
2015-11-20 13:58:38 +0700

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Teckchun on 11/20/15.
+//  Copyright © 2015 Teckchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Teckchun on 11/20/15.
+//  Copyright © 2015 Teckchun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
